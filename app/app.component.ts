780f1c1be159cde50829620b90f5cb539d1b8bf3
jgigliotti <justin.gigliotti@gmail.com>
2016-09-18 14:44:39 -0500

    Relocate logout

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 
+import { AuthenticationService } from './util/authentication.service';
 import { Login } from './login/login.component';
 
 @Component({
+    moduleId: module.id,
     selector: 'cookies-app',
-    template: `<router-outlet></router-outlet>`
+    styleUrls: ['app.css'],
+    templateUrl: 'app.html',
+    providers: [ AuthenticationService ]
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    constructor(private _authService: AuthenticationService,
+                private _router: Router) { }
+
+    logout() {
+         this._authService.deauthenticateUser();
+         this._router.navigate(['']);
+     }
+}
--- app/orders/orders.component.ts
+++ app/orders/orders.component.ts
@@ -2,26 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../util/authentication.service';
+import { CookieOrder } from '../util/util.service';
+
+import { OrdersService } from './orders.service';
 
 @Component({
     moduleId: module.id,
     selector: 'orders',
     styleUrls: ['orders.css'],
-    templateUrl: 'orders.html',
-    providers: [ AuthenticationService ]
+    templateUrl: 'orders.html'
 })
 export class Orders implements OnInit {
+    private orders: CookieOrder[] = [];
+
     constructor(private _authService: AuthenticationService,
                 private _router: Router) { }
 
     ngOnInit() {
         if(!this._authService.authenticatedUser()) {
             this._router.navigate(['']);
-        }
-     }
-
-     logout() {
-         this._authService.deauthenticateUser();
-         this._router.navigate(['']);
-     }
+        }       
+     }   
 }
