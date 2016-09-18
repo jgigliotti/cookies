978402de549d60b28b204b5431795f71c024cb1b
jgigliotti <justin.gigliotti@gmail.com>
2016-09-18 10:38:44 -0500

    Update user to something more appropriate

--- app/util/util.service.ts
+++ app/util/util.service.ts
@@ -2,4 +2,4 @@ export class User {
     constructor(public username: string, public password: string) {}
 }
 
-export const users = [ new User('justin', 'password') ];
+export const users = [ new User('cookie', 'monster') ];
